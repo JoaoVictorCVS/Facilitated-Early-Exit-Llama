513b0d92aa69e4e6aa4c3226d3df4137fd871f32
JoaoVictorCVS <sajoaovictor@usp.br>
2026-06-26 20:03:11 +0200

    Paths in evaluate.py

--- src/evaluate_sst2.py
+++ src/evaluate_sst2.py
@@ -77,7 +77,7 @@ class EvalConfig(GenericConfig):
             # EELlamma with confidence-based early exit
             "eellama_model": {
                 "model_id": "meta-llama/Llama-3.2-1B",
-                "adapter_path":"./models/llama-3-eellama-3p2-1B-layerskip/layer0",
+                "adapter_path":"home/joaosa/facilitated-early-exit-llama/models/llama-3-eellama-3p2-1B-layerskip/llama-3-eellama-3p2-1B-layerkip-adapter-sst2-final",
                 "enabled": True,
                 "exit_layers": [3, 7, 11, 15],
                 "ee_softmax_threshold": 0.9,
@@ -88,7 +88,7 @@ class EvalConfig(GenericConfig):
             # SkipDecode EELlamma
             "skipdecode_model": {
                 "model_id": "meta-llama/Llama-3.2-1B-Instruct",
-                "adapter_path": "./models/eellama-skipdecode-sst2-final",
+                "adapter_path": "home/joaosa/facilitated-early-exit-llama/models/eellama-skipdecode-sst2-final",
                 "enabled": True,
                 "exit_layers": [3, 7, 11, 15],
                 "untied_heads": False,
